54afe3856d08b7c8953392f25777a6d7a96144c0
muli3203 <muli3203@gmail.com>
2019-12-16 17:32:05 +0300

    added constructors and functions.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,54 @@
-var pizzaName = ["Tikka","Crocodile","hotdog","fatayer","Raspberry"];
-var pizzaSize = ["Large","Medium","Small"];
-var pizzaCrust = ["Double","Thin","Gluten"];
-var pizzzaTopping = ["Bell","Mushroom","Meat"];
+$(document).ready(function () {
+  $('#delivery label[for="pick-up"]').click(function () {
+    $("#location").hide()
+  })
+  $('#delivery label[for="deliver"]').click(function () {
+    $("#location").show()
+  })
+  $("form#build-pizza").submit(function (event) {
 
-function getName() {
-    var name = $(".name :selected").val();
-    return parseInt(name);
+    var name = $("#pizzaname :selected")
+    var size = $("#pizzasize :selected")
+    var crust = $("#pizzacrust :selected")
+    var topping = $("#pizzatopping :selected")
+
+    var number = parseInt($("input#number").val())
+    var delivery = $("#delivery :checked").val()
+
+    var location = $("#location").val()
+
+    var pizza = new Pizza(name, size, crust, topping, number)
+
+    // alert(pizza.size)
+    placeOrder(pizza, delivery)
+    event.preventDefault()
+  })
+})
+
+function Pizza(name, size, crust, topping, number) {
+  this.name = name;
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+  this.number = number;
 }
-function getSize() {
-    var size = $(".size :selected").val();
-    return parseInt(name);
+
+Pizza.prototype.orderPrice = function () {
+  var sizePrice = parseInt(this.size.val());
+  var crustPrice = parseInt(this.crust.val());
+  var toppingPrice = parseInt(this.topping.val())
+
+  var pizzaPrice = (sizePrice + crustPrice + toppingPrice) * this.number;
+  return pizzaPrice;
+}
+
+function placeOrder(pizza, delivery, location) {
+  var deliveryPrice = 300
+  var totalPrice = pizza.orderPrice()
+  if (delivery === "true") {
+    totalPrice += deliveryPrice
+    // alert("Delivery on point")
+  }
+  $("#output").html(totalPrice)
+
 }
